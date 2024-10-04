faa35b7ddd2a4223c36b86ab45ac5e494fb9d7b0
DevUzum <willianleite20@hotmail.com>
2024-10-03 21:46:03 -0300

    Add project files.

---
+++ ProductManagerMVC/Controllers/ProdutoController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductManagerMVC.Models;
+using ProductManagerMVC.Service;
+
+namespace ProductManagerMVC.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private readonly ProdutoService _produtoService;
+
+        public ProdutoController(ProdutoService produtoService)
+        {
+            _produtoService = produtoService;
+        }
+
+        public IActionResult Index()
+        {
+            var produtos = _produtoService.Obter();
+            return View(produtos);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Produto produto)
+        {
+            _produtoService.Inserir(produto);
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Produto produto)
+        {
+            _produtoService.Alterar(produto);
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            var produto = _produtoService.ObterPorId(id);
+            return View(produto);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var produto = _produtoService.ObterPorId(id);
+            return View(produto);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Produto produto)
+        {
+            _produtoService.Remover(produto.Id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ProductManagerMVC/Service/ProdutoService.cs
@@ -0,0 +1,45 @@
+﻿using ProductManagerMVC.Models;
+
+namespace ProductManagerMVC.Service
+{
+    public class ProdutoService
+    {
+        List<Produto> produtos = new List<Produto>();        
+
+        public List<Produto> Obter()
+        {
+            return produtos;
+        }
+
+        public void Inserir(Produto produto)
+        {
+            produtos.Add(new Produto()
+            {
+                Id = produtos.Count,
+                Nome = produto.Nome,
+                Preco = produto.Preco,
+                Categoria = produto.Categoria,
+                Estoque = produto.Estoque
+            });
+        }
+
+        public void Remover(int id)
+        {
+            produtos.RemoveAt(id);
+        }
+
+        public void Alterar(Produto produto)
+        {
+            var produtoObtido = produtos.FirstOrDefault(x => x.Id == produto.Id);
+            produtoObtido.Nome = produto.Nome;
+            produtoObtido.Preco = produto.Preco;
+            produtoObtido.Categoria = produto.Categoria;
+            produtoObtido.Estoque = produto.Estoque;
+        }
+
+        public Produto ObterPorId(int id)
+        {
+            return produtos.First(x => x.Id == id);
+        }
+    }
+}
